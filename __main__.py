44f20c67587e2b751a9c7796ece263e782be1bf9
thehxdev <hossein.khosravi.ce@gmail.com>
2024-05-24 11:50:33 +0330

    add resample handler and cleaner code

--- __main__.py
+++ __main__.py
@@ -3,7 +3,7 @@ import logging
 import ffmpeg
 import random as rand
 import config as conf
-from telegram import Update
+from telegram import Update, Message, Bot
 from telegram.ext import (
     filters,
     MessageHandler,
@@ -31,13 +31,6 @@ logging.basicConfig(
 
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    msg: str = 'This bot can make samples from your music and audio files! Just send me an audio file.'
-    chat_id: int = update.effective_chat.id
-    await context.bot.send_message(chat_id=chat_id, text=msg)
-
-
-
 async def ffmpeg_make_sample(input_path: str,
                              output_path:str,
                              duration: int) -> None:
@@ -50,16 +43,20 @@ async def ffmpeg_make_sample(input_path: str,
     ffmpeg.run(audio_output, quiet=True)
 
 
+
 def remove_file(path: str) -> None:
     if (os.path.exists(path)):
         os.remove(path)
 
 
-async def audio_sample_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    bot = context.bot
-    chat_id = update.effective_chat.id
-    msg = update.effective_message
-    file_name = msg.effective_attachment.file_name
+
+async def create_sample(bot: Bot, msg: Message, chat_id: int):
+    if msg.audio is None:
+        await msg.reply_text(text='Given message does not contain an audio file',
+                             reply_to_message_id=msg.id)
+        return
+
+    file_name = msg.audio.file_name
     input_path = f'{DIR_PREFIX}/{file_name}'
     output_path = f'{DIR_PREFIX}/new_{file_name}'
 
@@ -95,10 +92,39 @@ async def audio_sample_handler(update: Update, context: ContextTypes.DEFAULT_TYP
 
 
 
+async def start_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    msg: str = 'This bot can make samples from your music and audio files!\nJust send me an audio file.'
+    chat_id: int = update.effective_chat.id
+    await context.bot.send_message(chat_id=chat_id, text=msg)
+
+
+
+async def resample_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    bot = context.bot
+    chat_id = update.effective_chat.id
+    msg = update.effective_message.reply_to_message
+    if msg is None:
+        return
+    await create_sample(bot, msg, chat_id)
+
+
+
+async def audio_sample_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    bot = context.bot
+    chat_id = update.effective_chat.id
+    msg = update.effective_message
+    if msg is None:
+        return
+
+    await create_sample(bot, msg, chat_id)
+
+
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token(BOT_TOKEN).build()
 
-    application.add_handler(CommandHandler('start', start))
+    application.add_handler(CommandHandler('start', start_handler))
+    application.add_handler(CommandHandler('resample', resample_handler))
 
     application.add_handler(MessageHandler(filters.AUDIO & (~filters.COMMAND),
                                            audio_sample_handler, block=False))
