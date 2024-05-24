19af5fedef313c3414a57a0874bfdc40b93c4556
thehxdev <hossein.khosravi.ce@gmail.com>
2024-05-24 10:35:07 +0330

    bug fixes and better error handling

--- __main__.py
+++ __main__.py
@@ -2,17 +2,23 @@ import os
 import logging
 import ffmpeg
 import random as rand
+import config as conf
 from telegram import Update
-from telegram.ext import    \
-    filters, MessageHandler, ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import (
+    filters,
+    MessageHandler,
+    ApplicationBuilder,
+    ContextTypes,
+    CommandHandler
+)
 
 
 # Your telegram bot token
-BOT_TOKEN = ''
+BOT_TOKEN = conf.BOT_TOKEN
 
 # Where to download audio files and process them.
 # It MUST be a directory
-DIR_PREFIX = ''
+DIR_PREFIX = conf.DIR_PREFIX
 
 SAMPLE_DURATION = 30
 FINAL_AUDIO_BITRATE = 96
@@ -20,13 +26,13 @@ FINAL_AUDIO_BITRATE = 96
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    level=logging.WARN
 )
 
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    msg: str = 'This bot can make samples from your music and audio files!'
+    msg: str = 'This bot can make samples from your music and audio files! Just send me an audio file.'
     chat_id: int = update.effective_chat.id
     await context.bot.send_message(chat_id=chat_id, text=msg)
 
@@ -35,14 +41,18 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def ffmpeg_make_sample(input_path: str,
                              output_path:str,
                              duration: int) -> None:
-    start = rand.randint(0, duration - SAMPLE_DURATION)
+    start = (0) if duration == 0 else rand.randint(0, duration - SAMPLE_DURATION) 
     audio_input = ffmpeg.input(input_path)
     audio_cut = audio_input.audio.filter('atrim', start=start, end=start + SAMPLE_DURATION)
     audio_output = ffmpeg.output(audio_cut,
                                  output_path,
                                  **{'b:a': f'{FINAL_AUDIO_BITRATE}k'}).overwrite_output()
-    ffmpeg.run(audio_output)
+    ffmpeg.run(audio_output, quiet=True)
 
+
+def remove_file(path: str) -> None:
+    if (os.path.exists(path)):
+        os.remove(path)
 
 
 async def audio_sample_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -53,12 +63,11 @@ async def audio_sample_handler(update: Update, context: ContextTypes.DEFAULT_TYP
     input_path = f'{DIR_PREFIX}/{file_name}'
     output_path = f'{DIR_PREFIX}/new_{file_name}'
 
+    m = await msg.reply_text(text='Downloading file on server...',
+                             reply_to_message_id=msg.id)
+
     try:
         audio_file = await msg.audio.get_file()
-
-        m = await msg.reply_text(text='Downloading file on server...',
-                                 reply_to_message_id=msg.id)
-
         await audio_file.download_to_drive(input_path)
 
         await bot.edit_message_text(chat_id=chat_id,
@@ -71,12 +80,18 @@ async def audio_sample_handler(update: Update, context: ContextTypes.DEFAULT_TYP
                                     message_id=m.id,
                                     text='Sending sample audio...')
 
-        await msg.reply_voice(voice=output_path, reply_to_message_id=msg.id)
+        await msg.reply_voice(voice=output_path,
+                              reply_to_message_id=msg.id,
+                              caption=msg.audio.title)
 
         await m.delete()
+    except:
+        await bot.edit_message_text(chat_id=chat_id,
+                                    message_id=m.id,
+                                    text='Creating sample failed!')
     finally:
-        os.remove(input_path)
-        os.remove(output_path)
+        remove_file(input_path)
+        remove_file(output_path)
 
 
 
