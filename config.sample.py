bee30973e751f050be02cee5d03dd0dd813921ab
thehxdev <hossein.khosravi.ce@gmail.com>
2024-12-13 11:34:11 +0330

    added sample config file for muspler
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

---
+++ config.sample.py
@@ -0,0 +1,7 @@
+# Your telegram bot token
+BOT_TOKEN = 'YOUR_BOT_TOKEN'
+
+# Directory to download and process audio files temporarily. (MUST be a directory)
+# Downloaded audio files will be removed after sample creation process.
+DIR_PREFIX = 'TEMP_DIRECTORY'
+
